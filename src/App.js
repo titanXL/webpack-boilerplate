063a1c506d648fd125dbdd4832a3bad058a4c371
Emil <emildavidkov88@gmail.com>
2018-12-15 20:51:05 +0200

    Prettier

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { hot } from "react-hot-loader";
+import React from 'react'
+import { hot } from 'react-hot-loader'
 
-import "./styles.css";
+import './styles.css'
 
-const Warning = React.lazy(() => import("./Warning"));
+const Warning = React.lazy(() => import('./Warning'))
 
 class App extends React.Component {
   state = {
     count: 0
-  };
+  }
   render() {
-    const { count } = this.state;
+    const { count } = this.state
     return (
       <div>
-        <h1 className={count > 10 ? "warning" : ""}>Hello World! {count}</h1>
+        <h1 className={count > 10 ? 'warning' : ''}>Hello World! {count}</h1>
         <button
           onClick={() => this.setState(state => ({ count: state.count + 1 }))}
         >
@@ -30,8 +30,8 @@ class App extends React.Component {
           </React.Suspense>
         ) : null}
       </div>
-    );
+    )
   }
 }
 
-export default hot(module)(App);
+export default hot(module)(App)
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { render } from "react-testing-library";
+import React from 'react'
+import { render } from 'react-testing-library'
 
-import App from "./App";
+import App from './App'
 
-describe("App", () => {
-  it("Renders without error", () => {
-    render(<App />);
-  });
-});
+describe('App', () => {
+  it('Renders without error', () => {
+    render(<App />)
+  })
+})
