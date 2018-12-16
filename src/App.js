7af6203375b41a4fb6ce1076579081efd69b4561
Emil <emildavidkov88@gmail.com>
2018-12-16 17:42:57 +0200

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -2,37 +2,11 @@ import React from 'react'
 import { hot } from 'react-hot-loader'
 
 import './styles.css'
-import image from './image.jpg'
-
-const Warning = React.lazy(() => import('./Warning'))
-
 class App extends React.Component {
-  state = {
-    count: 0
-  }
-
   render() {
-    const { count } = this.state
     return (
       <div>
-        <img src={image} alt="logo" />
-        <h1 className={count > 10 ? 'warning' : ''}>Hello World! {count}</h1>
-        <h2>TEST</h2>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
-        >
-          Decrement
-        </button>
-        {count > 10 ? (
-          <React.Suspense fallback={null}>
-            <Warning />
-          </React.Suspense>
-        ) : null}
+        <h1>Hello World</h1>
       </div>
     )
   }
